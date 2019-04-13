a358cef59634a22d0b083f56da636b088aee2c0c
Fowod <noreasonforthisthing@gmail.com>
2019-04-13 09:40:08 -0400

    Smoothed player movement

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float speed = 100f;
+    public float speed;
     private Rigidbody2D rb2d;
-    // Start is called before the first frame update
+    private Vector2 moveVelocity;
     void Start()
     {
         rb2d = GetComponent <Rigidbody2D> ();
@@ -15,57 +15,11 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
-            rb2d.velocity = new Vector2(0.0f, speed * Time.deltaTime);
-            Debug.Log("up was pressed! :D");
-        }
-        /*if (Input.GetKeyUp(KeyCode.UpArrow)){
-            rb2d.velocity = new Vector2(0.0f, 0.0f);
-            Debug.Log("right arrow was released ;)");
-        }
-        */
-        if (Input.GetKeyDown(KeyCode.DownArrow)){
-            rb2d.velocity = new Vector2(0.0f, -speed * Time.deltaTime);
-            Debug.Log("down was pressed! :D");
-        }
-        /* if (Input.GetKeyUp(KeyCode.DownArrow)){
-            rb2d.velocity = new Vector2(0.0f, 0.0f);
-            Debug.Log("right arrow was released ;)");
-        }*/
-        
-        if (Input.GetKeyDown(KeyCode.LeftArrow)){
-            rb2d.velocity = new Vector2(-speed * Time.deltaTime, 0.0f);
-            Debug.Log("left was pressed! :D");
-        }
-        /* if (Input.GetKeyUp(KeyCode.LeftArrow)){
-            rb2d.velocity = new Vector2(0.0f, 0.0f);
-            Debug.Log("right arrow was released ;)");
-        }
-        */
-        if (Input.GetKeyDown(KeyCode.RightArrow)){
-            rb2d.velocity = new Vector2(speed * Time.deltaTime, 0.0f);
-            Debug.Log("right was pressed! :D");
-        }
-        /* if (Input.GetKeyUp(KeyCode.RightArrow)){
-            rb2d.velocity = new Vector2(0.0f, 0.0f);
-            Debug.Log("right arrow was released ;)");
-        }
-        */
-
-        if ((!Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow)) 
-        && (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))){
-            rb2d.velocity = new Vector2(0.0f, 0.0f);
-            Debug.Log("nothing is pressed :(");
-        }
-
+       Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       moveVelocity = moveInput.normalized * speed;
+    }
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
