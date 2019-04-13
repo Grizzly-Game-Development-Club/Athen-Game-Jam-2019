460b67ac9d04fd132b68a6f9a76c1ef695a8945d
supersand24 <supersand24@gmail.com>
2019-04-13 11:35:07 -0400

    User Interface

---
+++ Assets/score.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    //public score;
+    public Text scoreText;
+    public int enemyScoreEarned;
+    public int playerScore;
+
+
+    void addToScore(int scoreAdded)
+    {
+        playerScore = playerScore + enemyScoreEarned;
+        updateScoreGUI();
+    }
+
+    void updateScoreGUI()
+    {
+        scoreText.text = playerScore.ToString("0");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            addToScore(enemyScoreEarned);
+        }
+    }
+
+}
