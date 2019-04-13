f1d41ad3741095d3382cae1857fe9bba084e6c66
pilripso123 <47063854+pilripso123@users.noreply.github.com>
2019-04-12 22:47:49 -0400

    Started work on waypoint

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public int enemyWaypointID;
+    public int enemyHealth;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Attack()
+    {
+
+
+    }
+
+    public void Move(GameObject nextMarker)
+    {
+
+
+    }
+
+    public void Despawn()
+    {
+
+
+    }
+
+    public void Destory()
+    {
+
+
+    }
+
+}
