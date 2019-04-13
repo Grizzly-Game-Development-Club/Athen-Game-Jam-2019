728850097d575afd2477f660f049f6e22a93cd6b
supersand24 <supersand24@gmail.com>
2019-04-13 14:55:55 -0400

    Enemy Damage, Health Pickups

--- Assets/liveManager.cs
+++ Assets/liveManager.cs
@@ -5,29 +5,40 @@ using UnityEngine;
 public class liveManager : MonoBehaviour {
 
     public GameObject livesPrefabs;
-
+    public lifePickup lifePickup;
+    private int iframe = 0;
+    public int iframeSec = 2;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        for (int i=0;i< 3; i++)
+        {
+            addLive();
+        }
+    }
 
+	void Update () {
+        if (iframe > 0)
+        {
+            iframe = iframe - 1;
+        }
     }
 
     public void addLive()
     {
         GameObject newLives = Instantiate(livesPrefabs, this.transform) as GameObject;
-
     }
 
     public void removeLive()
     {
-        if (transform.childCount > 0)
+        if (iframe <= 0)
         {
-            GameObject lastChild = transform.GetChild(transform.childCount - 1).gameObject;
-            Destroy(lastChild);
+            if (transform.childCount > 0)
+            {
+                GameObject lastChild = transform.GetChild(transform.childCount - 1).gameObject;
+                Destroy(lastChild);
+                iframe = iframeSec * (int)Time.deltaTime;
+            }
         }
+
     }
 }
