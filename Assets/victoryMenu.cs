ee377eb9854f0563771ec0dae7439704064aab7b
pilripso123 <47063854+pilripso123@users.noreply.github.com>
2019-04-13 02:17:36 -0400

    winning screen

---
+++ Assets/victoryMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class victoryMenu : MonoBehaviour
+{
+    public void victory()
+    {
+        SceneManager.LoadScene("Main Menu");
+
+    }
+
+    public void restart()
+
+    {
+        SceneManager.LoadScene("Level One");
+    }
+}
