a1540c36e09b71f56f0985be5e797b555bed62b4
Fowod <noreasonforthisthing@gmail.com>
2019-04-13 23:04:07 -0400

    Miguel in progress pls help

---
+++ Assets/Script/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bomb : MonoBehaviour
+{
+    public GameObject[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton Down("Fire2"))
+        {
+            blowUp();
+            Debug.Log("hey that's pretty good");
+        }
+
+        void blowUp()
+        {
+           // var enemies = GameObject[] = GameObject.FindGameObjectsWithTag("Enemy");
+           enemies = GameObject.FindGameObjectsWithTag("Enemy");
+           for (int i = 0 ;i < enemies.Length; i++)
+           {
+               Debug.Log("somehow we got to this for loop god help us " + i + " times");
+           }
+        }
+    }
+}
